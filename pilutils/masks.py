2e5e8902d455c8d60568f5ef549ccf7a61455a09
supersebi3 <gdsupersebi3@gmail.com>
2020-07-30 13:16:01 +0200

    Change mask from mode 1 to mode L

--- pilutils/masks.py
+++ pilutils/masks.py
@@ -4,9 +4,9 @@ __all__ = ["ellipse"]
 
 
 def ellipse(size, invert=False):
-    """Returns a mask for an ellipse of the given size. The mask is an image with mode "1"."""
-    fg, bg = (0, 1) if invert else (1, 0)
-    mask = Image.new("1", size, bg)
+    """Returns a mask for an ellipse of the given size. The mask is an image with mode "L"."""
+    fg, bg = (0, 255) if invert else (255, 0)
+    mask = Image.new("L", size, bg)
     d = ImageDraw.Draw(mask)
     d.ellipse((0, 0, *size), fill=fg)
     return mask
