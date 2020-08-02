a60f0f4e95117994f2c59c17213f292024361e6a
supersebi3 <gdsupersebi3@gmail.com>
2020-08-02 10:55:45 +0200

    Add random mask

--- pilutils/masks.py
+++ pilutils/masks.py
@@ -1,5 +1,6 @@
 """Various functions to return alpha masks. All masks are mode L."""
 
+import random as _random
 from pilutils.basic import round_rectangle as draw_round_rectangle
 from PIL import Image, ImageDraw
 
@@ -28,3 +29,14 @@ def round_rectangle(size, radius, snap=True, invert=False):
     fg, bg = (bg, fg) if invert else (fg, bg)
     mask = draw_round_rectangle(size, radius, fg, bg, snap).convert("L")
     return mask
+
+
+def random(size, weight=0.5):
+    """Returns a randomized map where `weight` of the pixels are white and the rest are black. `weight = 0` means all pixels are black, `weight = 0.75` means 3/4 of pixels are white etc."""
+    img = Image.new("L", size)
+    for y in range(img.height):
+        for x in range(img.width):
+            p = _random.random()
+            if p < weight:
+                img.putpixel((x, y), 255)
+    return img
